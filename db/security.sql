1158224c71eb36bc7ea286eec072b115d17f0aa7
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-06-21 17:35:38 +0300

    Spring boot security [#296071]

---
+++ db/security.sql
@@ -0,0 +1,23 @@
+CREATE TABLE authorities
+(
+    id        serial primary key,
+    authority VARCHAR(50) NOT NULL unique
+);
+
+CREATE TABLE users
+(
+    id           serial primary key,
+    username     VARCHAR(50)  NOT NULL unique,
+    password     VARCHAR(100) NOT NULL,
+    enabled      boolean default true,
+    authority_id int          not null references authorities (id)
+);
+
+insert into authorities (authority)
+values ('ROLE_USER');
+insert into authorities (authority)
+values ('ROLE_ADMIN');
+
+insert into users (username, enabled, password, authority_id)
+values ('root', true, '$2a$10$3k8bnsScosCtG591rmSubeZwn3HOZwmN9O2nrYJFOD6Vh5Mo2I/AG',
+        (select id from authorities where authority = 'ROLE_ADMIN'));
--- src/main/java/forum/controller/LoginController.java
+++ src/main/java/forum/controller/LoginController.java
@@ -2,9 +2,15 @@ package forum.controller;
 
 import forum.model.User;
 import forum.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -44,6 +50,15 @@ public class LoginController {
         }
 
         return "auth/login";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
     }
 
 }
